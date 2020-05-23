03c79771961ebab7d5afc49658bee06d6465c444
Taya Kyouhei <taya_mary919@icloud.com>
2020-05-23 17:07:16 +0900

    seven

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,17 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
+    redirect_to user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
+    redirect_to root_path
+  end
   protected
 def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-  def set_current_user
-  @current_user = User.find_by(id: session[:user_id])
-end
-  private
-  def after_sign_in_path_for(resource)
-    flash[:notice] = "Signed in successfully."
-  	user_path(current_user.id)
-  end
-
-  def after_sign_out_path_for(resource)
-    flash[:notice] = "Signed out successfully."
-  	root_path
-  end
-   def ensure_correct_user
-    @book = Book.find_by(id:params[:id])
-    if @book.user_id = @current_user.id
-      flash[:notice] = "権限がありません"
-      redirect_to("/books/index")
-    end
-    end
 end
